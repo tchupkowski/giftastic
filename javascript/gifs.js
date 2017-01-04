bfe51ca7b03bbe2fb3f08098956b6e7414381cc5
tom c <tom c>
2017-01-03 19:16:27 -0500

    animates gifs and works for new buttons

--- javascript/gifs.js
+++ javascript/gifs.js
@@ -1,3 +1,9 @@
+//problems to fix
+	//all data is fish
+	
+	
+
+
 //Global Variables 
 var topics = ["mecury", "Venus", "Earth", "Mars", "Saturn", "space x", "nasa"];
 
@@ -12,7 +18,7 @@ function printButtons(){
 
 	for (i=0; i<topics.length; i++){
 
-		$("#spaceButtons").append("<button type='button' data-spaceP='fish' class='btn btn-primary'>" + topics[i] + "</button>");
+		$("#spaceButtons").append("<button type='button' data-spaceP=topics[i] class='btn btn-primary'>" + topics[i] + "</button>");
 
 
 	}
@@ -40,9 +46,11 @@ $("#add-space").on("click", function(event) {
 
 
 //this function returns gifs depending on which button you push 
-$(document).ready(function() {
+//$(document).ready(function() {
 
-$("button").on("click", function() {
+	$(document).on("click", ".btn-primary", function() {
+
+//$("button").on("click", function() {
 
 	//delete content 
 	$("#space-gifs").html("");
@@ -64,15 +72,39 @@ $("button").on("click", function() {
             var rating = results[i].rating;
             var p = $("<p>").text("Rating: " + rating);
             var personImage = $("<img>");
-            personImage.attr("src", results[i].images.fixed_height.url);
+            personImage.attr("src", results[i].images.fixed_height_still.url); 
+            personImage.attr("data-still", results[i].images.fixed_height_still.url);
+            personImage.attr("data-animate", results[i].images.fixed_height.url); 
+            personImage.attr("data-state", "still");
+            personImage.attr("class", "gif");
             gifDiv.prepend(p);
             gifDiv.prepend(personImage);
             $("#space-gifs").prepend(gifDiv);
           }
         });
-    });
+	
 });
+    
+//});
 
+//$(document).ready(function() {
+	$(document).on("click", ".gif", function() {
+
+	//$(".gif").on("click", function() {
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
+//});
 //Main Process 
 
 printButtons();
